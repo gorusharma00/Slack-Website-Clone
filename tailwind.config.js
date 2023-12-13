c75184cba6cf24ccfb81bf26e5d69aac9267c241
Gourav Sharma <ary6981@gmail.com>
2023-12-13 23:00:18 +0530

    Slack Website Clone is complete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        popins: ['Poppins'],
+        roboto: ['Roboto'],
+      },
+    },
+  },
+  plugins: [],
+}
+
